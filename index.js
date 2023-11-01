c53fbde23eb44b49eaab89cd34f1f7be674d49cc
Brandy <brandon-kah@live.com>
2023-11-02 06:00:28 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -13,46 +13,81 @@ module.exports = function NaslowRedirect(mod) {
 		pathone = new Vec3(12878, -24318, -134),
 		pathtwo = new Vec3(12494, -29119, 209)
 
-	let enabled = false
+	let enabled = false,
+		inTulu = false,
+		inAcarum = false,
+		inFron = false,
+		inFrontwo = false,
+		inBR = false,
+		inPath = false,
+		inPathtwo = false
 	
 	mod.hook('S_LOAD_TOPO', 3, event => {
-		inTulu = (event.zone === 7003)
-		inAcarum = (event.zone === 7012)
-		inFron = (event.zone === 2)
-		inFrontwo = (event.zone === 7013)
-		inBR = (event.zone === 7015)
-		inPath = (event.zone === 3)
-		inPathtwo = (event.zone === 7023)
+		if(event.zone === 7003) {
+			inTulu = true
+		} else {
+			inTulu = false
+		}
+		if(event.zone === 7012) {
+			inAcarum = true
+		} else {
+			inAcarum = false
+		}
+		if(event.zone === 2) {
+			inFron = true
+		} else {
+			inFron = false
+		}
+		if(event.zone === 7013) {
+			inFrontwo = true
+		} else {
+			inFrontwo = false
+		}
+		if(event.zone === 7015) {
+			inBR = true
+		} else {
+			inBR = false
+		}
+		if(event.zone === 3) {
+			inPath = true
+		} else {
+			inPath = false
+		}
+		if(event.zone === 7023) {
+			inPathtwo = true
+		} else {
+			inPathtwo = false
+		}
 	})
 	
 	mod.hook('S_SPAWN_ME', 3, event => {
 		if(!enabled) return
-        if(inTulu && (tuluone.dist3D(event.loc) <= 5 || tulutwo.dist3D(event.loc) <= 5)) {
-            event.loc = new Vec3(-64994, -35251, 1365)
+		if(inTulu && (tuluone.dist3D(event.loc) <= 5 || tulutwo.dist3D(event.loc) <= 5)) {
+			event.loc = new Vec3(-64994, -35251, 1365)
 			event.w = -1.43
 			setTimeout(function() { enabled = false }, 3000)
 			return true
 		}
-        if(inAcarum && (acaone.dist3D(event.loc) <= 5 || acatwo.dist3D(event.loc) <= 5)) {
-            event.loc = new Vec3(-29123, -74204, 6510) 
+		if(inAcarum && (acaone.dist3D(event.loc) <= 5 || acatwo.dist3D(event.loc) <= 5)) {
+			event.loc = new Vec3(-29123, -74204, 6510) 
 			event.w = 0.01
 			setTimeout(function() { enabled = false }, 3000)
 			return true
 		}
-        if((inFron || inFrontwo) && (fronone.dist3D(event.loc) <= 5 || frontwo.dist3D(event.loc) <= 5)) {
-            event.loc = new Vec3(-22804, 61231, 421)
+		if((inFron || inFrontwo) && (fronone.dist3D(event.loc) <= 5 || frontwo.dist3D(event.loc) <= 5)) {
+			event.loc = new Vec3(-22804, 61231, 421)
 			event.w = -2.12
 			setTimeout(function() { enabled = false }, 3000)
 			return true
 		}
 		if(inBR && cw.dist3D(event.loc) <= 5) {
-            event.loc = new Vec3(-13154, 88385, -1588)
+			event.loc = new Vec3(-13154, 88385, -1588)
 			event.w = 2.62
 			setTimeout(function() { enabled = false }, 3000)
 			return true
 		}
 		if((inPath || inPathtwo) && (pathone.dist3D(event.loc) <= 5 || pathtwo.dist3D(event.loc) <= 5)) {
-            event.loc = new Vec3(15449, -53895, -5266)
+			event.loc = new Vec3(15449, -53895, -5266)
 			event.w = 3.09
 			setTimeout(function() { enabled = false }, 3000)
 			return true
